83f878be7dc1e8489490eaeb17cf5761c9048b04
Angel Vea <vea_heredia92@hotmail.com>
2018-07-12 17:13:11 -0500

    Se agrego el endpoint para dar de alta multiples ciudades

---
+++ BookingMiddleware/Controllers/CitiesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BookingMiddleware.Database;
+using BookingMiddleware.Models;
+
+namespace BookingMiddleware.Controllers
+{
+    public class CitiesController : ApiController
+    {
+        private BookingDbContext db = new BookingDbContext();
+
+        [HttpPost]
+        [Route("api/cities/SaveCities")]
+        public IHttpActionResult SaveCities(List<City> _ciudaes)
+        {
+            foreach (var ciudad in _ciudaes)
+            {
+                City city = new City();
+                city.Id = ciudad.Id;
+                city.Name = ciudad.Name;
+                city.Country = ciudad.Country;
+                db.Cities.Add(city);
+                db.SaveChanges(); 
+
+            }
+
+            return Ok();
+        }
+
+
+        // GET: api/Cities
+        public IQueryable<City> GetCities()
+        {
+            return db.Cities;
+        }
+
+        // GET: api/Cities/5
+        [ResponseType(typeof(City))]
+        public IHttpActionResult GetCity(int id)
+        {
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city);
+        }
+
+        // PUT: api/Cities/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCity(int id, City city)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != city.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(city).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Cities
+        [ResponseType(typeof(City))]
+        public IHttpActionResult PostCity(City city)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cities.Add(city);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = city.Id }, city);
+        }
+
+        // DELETE: api/Cities/5
+        [ResponseType(typeof(City))]
+        public IHttpActionResult DeleteCity(int id)
+        {
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            db.Cities.Remove(city);
+            db.SaveChanges();
+
+            return Ok(city);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CityExists(int id)
+        {
+            return db.Cities.Count(e => e.Id == id) > 0;
+        }
+    }
+}
